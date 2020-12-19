84b134eb2efc550cd51bd475a749dc4d489893c8
play5330 <58069434+play5330@users.noreply.github.com>
2020-12-20 02:09:29 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,9 +14,9 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
-    if (message.content === '!공유') {
+    if (message.content === '!규칙') {
 
-       message.reply('https://discordapp.com/channels/533886195206324227/533886195722092555');
+       message.reply('여기는 성과를 올리는 채팅방입니다.\n그림, 글, 게임 등등의 성과를 올리면 됩니다.\n기만질에 해당하는 비틱질은 삭제됩니다.\n예)롤 펜타킬=비틱X, 가챠 10연차만에 한정뽑음 ㅎㅎ=비틱O');
 
        }
 
